33424100dc6764e93abd4fc99405bb6d37f5f970
iamwave007 <iamwave007@hotmail.com>
2014-10-11 18:32:39 -0700

    skeleton of DC
    hahaha

---
+++ select.js
@@ -0,0 +1,38 @@
+
+$(document).ready(function(){
+	$('.words').click(function(){
+		$(this).addClass('selected wds');
+	})
+})
+
+$(document).ready(function(){
+	$('.phrases').click(function(){
+		$(this).addClass('selected phrs');
+	})
+})
+
+
+$(document).ready(function(){
+	$('.idioms').click(function(){
+		$(this).addClass('selected idm');
+	})
+})
+
+
+$(document).ready(function(){
+	$('#words').click(function(){
+		alert("cool")
+	})
+})
+
+$(document).ready(function(){
+	$('#phrases').click(function(){
+		alert("cool")
+	})
+})
+
+$(document).ready(function(){
+	$('#idioms').click(function(){
+		alert("cool")
+	})
+})
