d8069b5c28581c44f7ca8cc1fa5ebbee39daf163
iamwave007 <iamwave007@hotmail.com>
2014-10-11 21:05:51 -0700

    write back to json
    input.html, input.js are files used to write raw data back to firebase

---
+++ input.js
@@ -0,0 +1,49 @@
+
+
+var boss=new Firebase('https://pracenglish.firebaseio.com/')
+
+
+
+var successPush=function(error) {
+  if (error) {
+    alert("Data could not be saved." + error);
+  } else {
+    alert("Data saved successfully.");
+  }
+}
+
+
+
+
+
+$(document).ready(function(){
+	$('#words').click(function(){
+		var currentFire=boss.child("selected").child("words")
+		$('.wds').each(function(){
+			var content=$(this).val()
+			currentFire.push(content)
+		})
+	})
+})
+
+$(document).ready(function(){
+	$('#phrases').click(function(){
+		var currentFire=boss.child("selected").child("phrases")
+		$('.phrs').each(function(){
+			var content=$(this).val()
+			currentFire.push(content)
+		})
+	})
+})
+
+$(document).ready(function(){
+	$('#idioms').click(function(){
+		var currentFire=boss.child("selected").child("idioms")
+		$('.idm').each(function(){
+			var content=$(this).val()
+			currentFire.push(content)
+		})
+	})
+})
+
+
