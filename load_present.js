1386badfeb416ea149ddc065738ba05347883037
iamwave007 <iamwave007@hotmail.com>
2014-10-12 08:21:16 -0700

    switch case
    using this to create table, not success. will be changed to 2-D array

--- load_present.js
+++ load_present.js
@@ -1,8 +1,4 @@
 
-
-
-
-
 var bank=new Firebase('https://pracenglish.firebaseio.com/selected')
 
 
@@ -10,16 +6,51 @@ $(document).ready(function(){
 	$("#loadBank").click(function(){
 		bank.on('value', function (snapshot) {
 		  var bnk = snapshot.val();
-		  console.log(bnk)
-		  for (var i in bnk){
-	  		console.log(i);
-	  		console.log(bnk[i]);
-	  		for (var j in bnk[i]){
-	  			//console.log(j); //this is keys
-	  			console.log(bnk[i][j]);
-	  		}}})
+		  //console.log(bnk)
+		  load_all(bnk)
+
+	  	})
 	})
 })
+
+
+var load_all=function(bnk){
+	
+	  for (var i in bnk){
+  		console.log("i is "+i);
+	  	switch(i) {
+	  		case "idioms":
+                load_words(bnk[i]);
+	  			break;
+	  		case "phrases":
+                load_words(bnk[i]);
+	  			break;
+	  		case "words":
+                load_words(bnk[i]);
+	  			break;
+	  		default:
+	  			alert("no match")
+	  	}
+  	}
+
+}
+
+
+var load_words=function(bnk){
+    var table = '<table>';
+
+    for(var j in bnk){
+        table += '<tr>'+
+        '<th>'+bnk[j]+'</th>'+
+        '<th>'+"Phrases"+'</th>'+
+        '<th>'+"Idioms"+'</th>'+
+        '</tr>';
+    }
+    table += '</table>';
+
+    document.getElementById('tableDiv').innerHTML = table;
+
+};
 
 
 
